418089d441ed47e9f3e1f9a0cf01715e1bd19de1
SHIMA Tomoya <kd131340@st.kobedenshi.ac.jp>
2023-10-03 12:25:31 +0900

    提出

--- ex2/work.py
+++ ex2/work.py
@@ -2,7 +2,22 @@ from math import ceil
 
 def calc_account(m):
     # 実装は入れていません、自分で入れてください
-    pass
+    if m <= 0:
+        return None
+    
+    #初乗りの距離までの料金
+    min_kyori = 1700
+    min_sum = 610
+
+    #初乗りの距離を超える場合
+    if m <= min_kyori:
+        return min_sum
+    else:
+        #追加料金
+        add_kyori=m - min_kyori
+        add_sum=ceil(add_kyori/315)*80
+        total_min=min_sum + add_sum
+        return total_min
 
 if __name__ == "__main__":
     from argparse import ArgumentParser
